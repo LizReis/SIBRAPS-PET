a070408bdd6737c579a8c5d861d51ec67040a593
Liz <124801714+LizReis@users.noreply.github.com>
2026-08-06 00:21:52 +0000

    Add import que faltava do localDateTime

--- backend/buscaativa/src/main/java/com/pet/buscaativa/repositories/AgendamentoRepository.java
+++ backend/buscaativa/src/main/java/com/pet/buscaativa/repositories/AgendamentoRepository.java
@@ -2,6 +2,7 @@ package com.pet.buscaativa.repositories;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.time.LocalTime;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
